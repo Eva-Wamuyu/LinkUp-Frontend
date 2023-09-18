bbba3cc4b1494ade2145af73027bb2b414f5939d
Eva-Wamuyu <rutheve.eva@gmail.com>
2023-09-18 14:20:19 +0300

    add prod link

--- src/app/services/interfaces.ts
+++ src/app/services/interfaces.ts
@@ -1,4 +1,5 @@
-export const BASE_URL  = 'http://localhost:3000/'
+// export const BASE_URL  = 'http://localhost:3000/'
+export const BASE_URL = 'https://wamuyu-linkup-api.azurewebsites.net/'
 
 export interface RegisterDetails{
   usernames: string
